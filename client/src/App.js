440a96c526a27ab081c86c116c5a6edb08465634
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-22 14:33:30 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -21,12 +21,21 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <h1>Welcome to Blue Ocean!</h1>
+        {/* <h1>Welcome to Blue Ocean!</h1>
         <ul>
           {this.state.seaCreatures.map((creature, index) => (
             <li key={index}>{creature}</li>
+            
           ))}
-        </ul>
+        </ul> */}
+
+      <div className="flx-embed">
+      <div style={{position:"relative", height:"0", paddingBottom:"56.22%",width:"100%"}}>
+      <iframe style={{pointerEvents: "none", position: "absolute", top:"0", left:"0", width:"100%", height:"100%"}} src="https://media.flixel.com/cinemagraph/llnshyphvx0lmkz6ceqq?hd=true" frameBorder="0" allowFullScreen allow='autoplay'></iframe>
+      </div>
+      </div>
+        
+       
       </>
     );
   }
