e23189950c2f996d2a49a6ae1f42dd8c4ca9dd5a
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-22 15:53:07 -0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -5,32 +5,37 @@ const app = express();
 
 
 // open up CORS 
-app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use((_, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
-app.use(logger('dev'));
+app.use(express.static('../client/public'));
+// app.use(logger('dev'));
 
-// You can place your routes here, feel free to refactor:
-const { example } = require('./routes');
-app.use('/api/example', example)
+// // You can place your routes here, feel free to refactor:
+// const { example } = require('./routes');
+// app.use('/api/example', example)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
+
+app.listen(8000, ()=>{console.log('listenining at port 8000')})
+
 
 module.exports = app;
