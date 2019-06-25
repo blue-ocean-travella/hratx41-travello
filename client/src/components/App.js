fc2d712116613176b6b2c08d993087a939cc41a2
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-25 16:14:31 -0500

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import fetch from 'node-fetch';
-// import './App.css';
 
 export default class App extends Component {
   constructor(props) {
@@ -8,34 +7,20 @@ export default class App extends Component {
     this.state = {
       seaCreatures: []
     };
-    // this.api = `http://localhost:8000/api/example`;
+
   }
   componentDidMount() {
-    // fetch(this.api)
-    //   .then(res => res.json())
-    //   .then(seaCreatures => {
-    //     this.setState({ seaCreatures: seaCreatures.data });
-    //   });
+
   }
 
   render() {
     return (
       <>
-        {/* <h1>Welcome to Blue Ocean!</h1>
-        <ul>
-          {this.state.seaCreatures.map((creature, index) => (
-            <li key={index}>{creature}</li>
-            
-          ))}
-        </ul> */}
-
       <div className="flx-embed">
       <div style={{position:"relative", height:"0", paddingBottom:"56.22%",width:"100%"}}>
       <iframe style={{pointerEvents: "none", position: "absolute", top:"0", left:"0", width:"100%", height:"100%"}} src="https://media.flixel.com/cinemagraph/llnshyphvx0lmkz6ceqq?hd=true" frameBorder="0" allowFullScreen allow='autoplay'></iframe>
       </div>
       </div>
-        
-       
       </>
     );
   }
