1983d606aa2b8501d83a88815a0f8ed17f91152f
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-25 14:43:54 -0500

    ok

--- server/server.js
+++ server/server.js
@@ -4,36 +4,14 @@ const express = require('express');
 const app = express();
 
 
-// open up CORS 
-// app.use((_, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+express.static('../client/public');
 
-//hello;
-app.use(express.static('../client/public'));
-// app.use(logger('dev'));
 
-// // You can place your routes here, feel free to refactor:
-// const { example } = require('./routes');
-// app.use('/api/example', example)
 
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     next(createError(404));
-// });
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, ()=>{console.log(`listening at ${port}`)})
