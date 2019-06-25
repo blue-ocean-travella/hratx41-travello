c0755711aac2ce84f955e4dac62bc87f50b03dac
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-25 10:22:31 -0500

    commit

---
+++ client/src/testdata.js
@@ -0,0 +1,45 @@
+
+ var restaurant = {
+     "formatted_address" : "3755 S Capital of Texas Hwy B, Austin, TX 78704, USA",
+     "geometry" : {
+        "location" : {
+           "lat" : 30.24716699999999,
+           "lng" : -97.81268919999999
+        },
+        "viewport" : {
+           "northeast" : {
+              "lat" : 30.24851682989272,
+              "lng" : -97.81133937010726
+           },
+           "southwest" : {
+              "lat" : 30.24581717010727,
+              "lng" : -97.81403902989271
+           }
+        }
+     },
+     "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_recreational-71.png",
+     "id" : "51fd876e6889013fb5b8905855966e0ae5c43d1a",
+     "name" : "Barton Creek Greenbelt",
+     "opening_hours" : {
+        "open_now" : true
+     },
+     "photos" : [
+        {
+           "height" : 3024,
+           "html_attributions" : [
+              "\u003ca href=\"https://maps.google.com/maps/contrib/107606232986293114286/photos\"\u003eWill Tucker\u003c/a\u003e"
+           ],
+           "photo_reference" : "CmRaAAAAl33vUqnBcad7zKGqG6E9NR3HmlS1_dWfWR1YrvTvlQFKD3a4bjogSeZcTIZ8ZVXzxpxxdz6m7WXlzY-_h_c1IIUHVAeOuX8MQ0OsdzslREpYM4PxDmYwOjcQaPirG6qwEhAynmBLqk6QtpWC_AvXEzwsGhTKZPRvwsKib9E2Qyfu2u6JSO0Wiw",
+           "width" : 4032
+        }
+     ],
+     "place_id" : "ChIJabb0sdZKW4YR7Rlal52tALI",
+     "plus_code" : {
+        "compound_code" : "65WP+VW Austin, Texas, USA",
+        "global_code" : "862465WP+VW"
+     },
+     "rating" : 4.7,
+     "reference" : "ChIJabb0sdZKW4YR7Rlal52tALI",
+     "types" : [ "park", "point_of_interest", "establishment" ],
+     "user_ratings_total" : 2465
+  }
