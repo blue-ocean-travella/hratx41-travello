819d5094e8028b61437c11668405013a960eb7f7
Gladys Benitez <gladysbenitez.an@gmail.com>
2019-06-25 12:56:56 -0500

    update search bar and home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,50 +1,35 @@
 import React, { Component } from 'react';
-import fetch from 'node-fetch';
-// import './App.css';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'; 
+import SearchBar from './searchBar/searchBar.jsx';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seaCreatures: [],
-      play: true
-    };
-    // this.api = `http://localhost:8000/api/example`;
-    this.url  =`ocean.mp3`;
-    this.audio = new Audio(this.url);
-    
-  }
-  componentDidMount() {
-    // fetch(this.api)
-    //   .then(res => res.json())
-    //   .then(seaCreatures => {
-    //     this.setState({ seaCreatures: seaCreatures.data });
-    //   });
-
-    this.audio.play();
-    console.log('running');
-    
+      data:[],
+      query:''
+    }; 
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  onInputChange(event){
+    console.log(event.target.value, 'this is what the user typed');
+    this.setState({ query: event.target.value });
+  }; 
+  onSubmit(event){ 
+    if (event.key === 'Enter') {  
+      this.initialize();
+        console.log(this.state.query,'a city was submitted');
+    }
+  }
   render(){
     return (
       <>
-        {/* <h1>Welcome to Blue Ocean!</h1>
-        <ul>
-          {this.state.seaCreatures.map((creature, index) => (
-            <li key={index}>{creature}</li>
-            
-          ))}
-        </ul> */}
-
-      <div className="flx-embed">
-      <div style={{position:"relative", height:"0", paddingBottom:"56.22%",width:"100%"}}>
-      <iframe style={{pointerEvents: "none", position: "absolute", top:"0", left:"0", width:"100%", height:"100%"}} src="https://media.flixel.com/cinemagraph/llnshyphvx0lmkz6ceqq?hd=true" frameBorder="0" allowFullScreen allow='autoplay'></iframe>
-      </div>
-      </div>
-        
-       
-      </>
+     <SearchBar onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+     </>
     );
   }
 }
+
