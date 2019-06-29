6b6f0941e460a107c55f6e807c5f895690a6d770
Maria Jose <mariajosenc89@gmail.com>
2019-06-29 10:38:34 -0500

    class name is change, navigationBar are working

--- client/src/components/navigationBar/navigationBar.jsx
+++ client/src/components/navigationBar/navigationBar.jsx
@@ -15,11 +15,11 @@ const NavigationBar = ({onCategoryChange, currentCategory}) => {
   return (
     <>
       <div className='navigationBar'>
-        <button className={`category ${currentCategory === 'topSpots' ? 'changeCurrentCategory' : ''}`} onClick={() => onCategoryChange('topSpots')}>TOP SPOTS</button>
-        <button className={`category ${currentCategory === 'thingsToDo' ? 'changeCurrentCategory' : ''}`} onClick={() => onCategoryChange('thingsToDo')}>THINGS TO DO</button>
-        <button className={`category ${currentCategory === 'restaurants' ? 'changeCurrentCategory' : ''}`} onClick={() => onCategoryChange('restaurants')}>FOOD {'&'} DRINK</button>
-        <button className={`category ${currentCategory === 'nightLife' ? 'changeCurrentCategory' : ''}`} onClick={() => onCategoryChange('nightLife')}>NIGHTLIFE</button>
-        <button className={`category ${currentCategory === 'dayTrips' ? 'changeCurrentCategory' : ''}`} onClick={() => onCategoryChange('dayTrips')}>DAY TRIPS</button>     
+        <button className={`category ${currentCategory === 'topSpots' ? 'changeClickedCurrentCategory' : ''}`} onClick={() => onCategoryChange('topSpots')}>TOP SPOTS</button>
+        <button className={`category ${currentCategory === 'thingsToDo' ? 'changeClickedCurrentCategory' : ''}`} onClick={() => onCategoryChange('thingsToDo')}>THINGS TO DO</button>
+        <button className={`category ${currentCategory === 'restaurants' ? 'changeClickedCurrentCategory' : ''}`} onClick={() => onCategoryChange('restaurants')}>FOOD {'&'} DRINK</button>
+        <button className={`category ${currentCategory === 'nightLife' ? 'changeClickedCurrentCategory' : ''}`} onClick={() => onCategoryChange('nightLife')}>NIGHTLIFE</button>
+        <button className={`category ${currentCategory === 'dayTrips' ? 'changeClickedCurrentCategory' : ''}`} onClick={() => onCategoryChange('dayTrips')}>DAY TRIPS</button>     
       </div>
     </>
   );   
