d8ebda3a36bfd79485b039f3b07a55406d41f1c0
Jimmy Hung <hungmeenhong@gmail.com>
2019-07-01 22:13:21 -0500

    big merge with david without app2.js and index2.html

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,20 +2,11 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/src');
 const DIST_DIR = path.join(__dirname, '/public');
 const Dotenv = require('dotenv-webpack');
-<<<<<<< HEAD
-=======
-
->>>>>>> itinerary
 module.exports = {
   entry: {search:`${SRC_DIR}/index.js`, itinerary:`${SRC_DIR}/index2.js`, connect:`${SRC_DIR}/index3.js`},
   output: {
-<<<<<<< HEAD
   filename: '[name].bundle.js',
   path: DIST_DIR
-=======
-    filename: 'bundle.js',
-    path: DIST_DIR
->>>>>>> itinerary
   },
   module: {
     rules: [
